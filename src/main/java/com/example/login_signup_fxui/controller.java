d70ed8a9177d5a14b29fbd498d8a64d018e4a3e0
KaungMyat <kaungmyat77.km99@gmail.com>
2021-10-09 08:38:45 +0630

    v-1.1

---
+++ src/main/java/com/example/login_signup_fxui/controller.java
@@ -0,0 +1,37 @@
+package com.example.login_signup_fxui;
+
+
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class controller implements Initializable {
+   
+    public AnchorPane root;
+    public HBox hb;
+
+
+    //    @Override
+    public void initialize(URL url, ResourceBundle resources) {
+//        TranslateTransition t1= new TranslateTransition(Duration.seconds(1),vb);
+////        TranslateTransition t2= new TranslateTransition(Duration.seconds(1),vb);
+//        t1.setToX(vb.getLayoutX()-600);
+//        System.out.println(vb.getLayoutX()-600);
+////        t2.setToX(p2.getLayoutX());
+//        t1.play();
+////        t2.play();
+    }
+////    @FXML
+////    void right(MouseEvent e){
+////            TranslateTransition t= new TranslateTransition(Duration.seconds(2),vb);
+////            t.setToX(vb.getLayoutX()+(root.getPrefWidth()- vb.getPrefWidth()));
+////            t.play();
+////    }
+}
