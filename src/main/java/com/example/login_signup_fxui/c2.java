90f3b570b5d77655948f7311414e1d48af829502
KaungMyat <kaungmyat77.km99@gmail.com>
2021-10-12 02:00:28 +0630

    V-1.3

---
+++ src/main/java/com/example/login_signup_fxui/c2.java
@@ -0,0 +1,4 @@
+package com.example.login_signup_fxui;
+
+public class c2 {
+}
--- src/main/java/com/example/login_signup_fxui/controller.java
+++ src/main/java/com/example/login_signup_fxui/controller.java
@@ -2,36 +2,77 @@ package com.example.login_signup_fxui;
 
 
 import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.util.Duration;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class controller implements Initializable {
-   
+public class controller{
+    public AnchorPane vb;
     public AnchorPane root;
-    public HBox hb;
+    public Pane p1;
+    public AnchorPane a1;
+    public AnchorPane a2;
+    public Button Swbtn;
+    private boolean sw=false;
+    @FXML
+    void left(ActionEvent e1) {
+        if (sw == false) {
+            TranslateTransition t1 = new TranslateTransition(Duration.seconds(1.5), vb);
+            TranslateTransition t2 = new TranslateTransition(Duration.seconds(1.5), p1);
+            t1.setToX(vb.getLayoutX() + (root.getPrefWidth() - vb.getPrefWidth()) + 30);
+            t2.setToX(-p1.getPrefWidth() + 100);
+            t1.play();
+            t2.play();
+            t2.setOnFinished((e) -> {
+                try {
+                    FXMLLoader fxml = new FXMLLoader(main.class.getResource("signUp.fxml"));
+                    FXMLLoader fxml1 = new FXMLLoader(main.class.getResource("wLogin.fxml"));
+                    a1 = fxml.load();
+                    a2 = fxml1.load();
+                    p1.getChildren().removeAll();
+                    p1.getChildren().setAll(a1);
+                    Swbtn.setText("Sign In");
+                    sw=true;
+                    vb.getChildren().set(0, a2);
 
+                } catch (Exception ex) {
+                    System.out.println(ex);
+                }
+            });
+        }else{
+            TranslateTransition t1 = new TranslateTransition(Duration.seconds(1.5), vb);
+            TranslateTransition t2 = new TranslateTransition(Duration.seconds(1.5), p1);
+            t1.setToX(0);
+            t2.setToX(0);
+            t1.play();
+            t2.play();
+            t2.setOnFinished((e2) -> {
+                try {
+                    FXMLLoader fxml = new FXMLLoader(main.class.getResource("signIn.fxml"));
+                    FXMLLoader fxml1 = new FXMLLoader(main.class.getResource("wSignUp.fxml"));
+                    a1 = fxml.load();
+                    a2 = fxml1.load();
+                    p1.getChildren().removeAll();
+                    p1.getChildren().setAll(a1);
+                    Swbtn.setText("Sign Up");
+                    sw=false;
+                    vb.getChildren().set(0, a2);
 
-    //    @Override
-    public void initialize(URL url, ResourceBundle resources) {
-//        TranslateTransition t1= new TranslateTransition(Duration.seconds(1),vb);
-////        TranslateTransition t2= new TranslateTransition(Duration.seconds(1),vb);
-//        t1.setToX(vb.getLayoutX()-600);
-//        System.out.println(vb.getLayoutX()-600);
-////        t2.setToX(p2.getLayoutX());
-//        t1.play();
-////        t2.play();
+                } catch (IOException ex) {
+                    System.out.println(ex);
+                }
+            });
+        }
     }
-////    @FXML
-////    void right(MouseEvent e){
-////            TranslateTransition t= new TranslateTransition(Duration.seconds(2),vb);
-////            t.setToX(vb.getLayoutX()+(root.getPrefWidth()- vb.getPrefWidth()));
-////            t.play();
-////    }
+
 }
